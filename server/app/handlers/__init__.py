721d0a39fa6a89e5e32360558b105f1ed96b53cf
joegasewicz <joegasewicz@gmail.com>
2025-01-08 18:18:05 +0000

    v2.1.3 Release

--- server/app/handlers/__init__.py
+++ server/app/handlers/__init__.py
@@ -1,3 +1,3 @@
+from app.handlers.base import BaseHandler
 from app.handlers.health import HealthHandler
 from app.handlers.locations import LocationHandler
-from app.handlers.base import BaseHandler
--- server/app/handlers/base.py
+++ server/app/handlers/base.py
@@ -5,8 +5,9 @@ class BaseHandler(tornado.web.RequestHandler):
 
     def set_default_headers(self) -> None:
         self.set_header("Access-Control-Allow-Origin", "*")
-        self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header("Access-Control-Allow-Credentials", "true")
+        self.set_header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+        self.set_header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
 
     def options(self):
         self.set_status(204)
